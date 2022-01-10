6556fdb2016a0e10d00b414020e396f626ef93fd
Adson Eduardo <adson458@hotmail.com>
2022-01-10 16:43:45 -0300

    create controller and routes for user

--- index.js
+++ index.js
@@ -20,66 +20,11 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-// config controllers
-const UserController = require("./controllers/UserController");
-const BookController = require("./controllers/BookController");
+const routes = require('./routes');
+app.use('/api', routes);
 
-//config routes
-app.get("/", function (req, res) {
-    res.render('login');
-});
-app.get("/register", function (req, res) {
-    res.render('register', {user: [{}]});
-});
-app.post("/register", function (req, res) {
-    new UserController().create(req, res);
-});
-app.get("/home", function (req, res) {
-    res.render('home');
-});
+//config logging
+const logger = require('morgan');
+app.use(logger('dev'))
 
-
-//users
-app.get("/users", function (req, res) {
-    new UserController().show(req, res);
-});
-app.get("/users/delete/:id", function (req, res) {
-    console.log(req.params.id);
-    new UserController().delete(req, res);
-})
-app.get("/users/show/:id", function (req, res) {
-    new UserController().show(req, res);
-})
-app.get("/users/edit/:id", function (req, res) {
-    new UserController().edit(req, res);
-});
-app.post("/users/edit", function (req, res) {
-    new UserController().update(req, res);
-});
-
-
-//books
-app.get("/books", function (req, res) {
-    new BookController().show(req, res);
-});
-app.get("/books/create", function (req, res) {
-    res.render('./book/create', {book: [{}]});
-});
-app.post("/books/create", function (req, res) {
-    new BookController().create(req, res);
-});
-app.get("/books/delete/:id", function (req, res) {
-    new BookController().delete(req, res);
-})
-app.get("/books/show/:id", function (req, res) {
-    new BookController().show(req, res);
-})
-app.get("/books/edit/:id", function (req, res) {
-    new BookController().edit(req, res);
-});
-app.post("/books/edit", function (req, res) {
-    new BookController().update(req, res);
-});
-
-//start app in a local port
 app.listen(8081);
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const controllers = require('../controllers/UserController');
+
+const router = Router();
+
+router.get('/', (req, res) => res.send('This is root!'));
+
+router.get('/users', controllers.getAllUsers)
+router.post('/users', controllers.createUser)
+
+router.get('/users/:id', controllers.getUserById)
+
+router.put('/users/:id', controllers.updateUser)
+router.delete('/users/:id', controllers.deleteUser)
+
+module.exports = router
