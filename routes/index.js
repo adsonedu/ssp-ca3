2111a9f2273c3a0ce87818aef4d42e7e725767bb
Adson Eduardo <adson458@hotmail.com>
2022-01-11 12:11:30 -0300

    configure flash messages

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ const passport = require('passport');
 
 const router = Router();
 
+const {authentication} = require('../helpers/authentication');
+
 //routes for login
 router.get("/", function (req, res) {
     res.render('login');
@@ -18,27 +20,27 @@ router.post("/", function (req, res, next) {
     })(req, res, next)
 })
 //routes for users
-router.get('/users', user.getAllUsers)
-router.post('/users', user.createUser)
+router.get('/users', authentication, user.getAllUsers)
+router.post('/users', authentication, user.createUser)
 router.get('/users/register', user.registerUser)
-router.get('/users/show/:id', user.getUserById)
-router.get('/users/edit/:id', user.updateUser)
-router.get('/users/delete/:id', user.deleteUser)
+router.get('/users/show/:id', authentication, user.getUserById)
+router.get('/users/edit/:id', authentication, user.updateUser)
+router.get('/users/delete/:id', authentication, user.deleteUser)
 
 //routes for subjects
 router.get('/subjects', subject.getAllSubjects)
-router.get('/subjects/create', subject.newSubject)
-router.post('/subjects/create', subject.createSubject)
-router.get('/subjects/show/:id', subject.getSubjectById)
-router.get('/subjects/edit/:id', subject.updateSubject)
-router.get('/subjects/delete/:id', subject.deleteSubject)
+router.get('/subjects/create', authentication, subject.newSubject)
+router.post('/subjects/create', authentication, subject.createSubject)
+router.get('/subjects/show/:id', authentication, subject.getSubjectById)
+router.get('/subjects/edit/:id', authentication, subject.updateSubject)
+router.get('/subjects/delete/:id', authentication, subject.deleteSubject)
 
 //routes for topics
 router.get('/topics', topic.getAllTopics)
-router.get('/topics/create', topic.newTopic)
-router.post('/topics/create', topic.createTopic)
-router.get('/topics/show/:id', topic.getTopicById)
-router.get('/topics/edit/:id', topic.updateTopic)
-router.get('/topics/delete/:id', topic.deleteTopic)
+router.get('/topics/create', authentication, topic.newTopic)
+router.post('/topics/create', authentication, topic.createTopic)
+router.get('/topics/show/:id', authentication, topic.getTopicById)
+router.get('/topics/edit/:id', authentication, topic.updateTopic)
+router.get('/topics/delete/:id', authentication, topic.deleteTopic)
 
 module.exports = router
